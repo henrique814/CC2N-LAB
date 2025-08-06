4f23353a0c6262a8b0926fc7001c475b8ce2daa2
henrique814 <hmendesamorim@gmail.com>
2025-08-05 22:22:55 -0300

    Add files via upload

--- E1/main.c
+++ E1/main.c
@@ -9,7 +9,7 @@ int main()
     int N1,N2;
     printf("MINI CALCULADORA DE INTEIROS: N1 e N2\n");
     printf("VALOR DE N1: ");
-    scanf("%i", &N1  );
+    scanf("%i", &N1);
     printf("VALOR DE N2: ");
     scanf("%i", &N2);
     printf("ADIÇÃO: N1 + N2 = %i + %i = %i\n" , N1, N2, N1 + N2);
@@ -21,4 +21,5 @@ int main()
     printf("RESTO: N1 mod N2 = %i %% %i = %i\n" , N1, N2, N1 % N2);
     printf("POTENCIAÇÃO: N1 elevado a N2 = %i elevado a %i = %.1f\n" , N1, N2, pow(N1,N2));
     printf("RADICIAÇÃO: N1 raiz N2 = %i raiz %i = %.1f\n" , N1, N2, pow(N1,1.0/N2));
+    return 0
 }
---
+++ Lex1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int B,C;
+    printf("Exercicio 2: \n\n"
+           "a) Hipotenusa (A) de um triangulo ABC: \n\n");
+           printf("B = ");
+           scanf("%i", &B);
+           printf("C = ");
+           scanf("%i", &C);
+           printf("A = %i",pow(pow(B,2) + pow(C,2),1.0/2);
+
+    return 0;
+}
