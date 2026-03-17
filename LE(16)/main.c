ca6c5febb048bae9a4e5933a3ba873fe81483df9
henrique814 <hmendesamorim@gmail.com>
2026-03-17 20:39:15 -0300

    Add files via upload

---
+++ LE(16)/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define INFINTY
+int main()
+{
+    int contador = 0;
+    float MaiorPar = -INFINTY, MenorPar = +INFINTY, MaiorImpar = -INFINTY, MenorImpar = +INFINTY;
+    printf("Maiores e Menores pares e impares:\n");
+    while(contador < 300){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        contador++;
+    }
+    return 0;
+}
