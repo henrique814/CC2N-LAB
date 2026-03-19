0a47c5c7901cd2da1a3c91d591b2040837b9ac50
henrique814 <hmendesamorim@gmail.com>
2026-03-19 20:35:49 -0300

    Add files via upload

---
+++ LE(24)/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int contador = 0;
+    float preco, reajusteV = 0;
+    printf("Preco de produtos com 5%% de reajuste\n");
+    while(contador < 50){
+        printf("preco da mercadoria %i: R$", contador + 1);scanf("%f", &preco);
+        reajusteV = preco * (5/100);
+        if(reajusteV <= 25.50){
+            printf("produto com desconto de 5%% = %.2f\n", preco - reajusteV);
+            printf("produto com acrescimo de 5%% = %.2f\n", preco + reajusteV);
+        }
+        else if(reajusteV >= 25.50){
+            printf("Reajuste muito grande: reajuste reajustado\n");
+                reajusteV = reajusteV - (2/100);
+            printf("produto com desconto de 5%% = %.2f\n", preco - reajusteV);
+            printf("produto com acrescimo de 5%% = %.2f\n", preco + reajusteV);
+        }
+    }
+    return 0;
+}
