d5c263247e45c157e812252c38ceff519ee464c9
henrique814 <hmendesamorim@gmail.com>
2026-04-23 20:42:37 -0300

    Add files via upload

--- (2)Ex/main.c
+++ (2)Ex/main.c
@@ -5,7 +5,7 @@
 int main()
 {
     float nota[TAMANHO], soma = 0, media = 0;
-    int indice = 0, i, matricula[TAMANHO], opcao, achar = 0, contador = 0, output;
+    int indice = 0, i, j,  matricula[TAMANHO], opcao, acharS = 0,acharB = 0, contador = 0, output, aux1 = 0, aux2 = 0,flag4 = 0, li, ls, meio = 0;
     printf("Cadastrar 50 alunos\n");
     do{
         printf("MENU:\n\n");
@@ -33,31 +33,66 @@ int main()
                 printf("ERRO NA NOTA\n");
             }while(nota[indice] < 0 || nota[indice] > 10);
         indice++;
-        }else
-        printf("ERRO turma cheia");
-        }else if(opcao == 2){
+        }else{
+        printf("ERRO turma cheia");}
+        flag4 =0;
+        }
+        else if(opcao == 2){
             soma = 0;
             for(i = 0;i <= indice;i++){
                 soma = soma + nota[i];
             }
             media = soma / indice;
             printf("media do bimestre entre os alunos: %.1f\n", media);
-        }else if(opcao == 3){
+        }
+        else if(opcao == 3){
             output = -1;
             contador = 0;
-            printf("digite uma matricula p/ procurar: ");scanf("%i", &achar);
+            printf("digite uma matricula p/ procurar: ");scanf("%i", &acharS);
             while(contador < indice){
-                if(matricula[contador] == achar){
+                if(matricula[contador] == acharS){
                         output = contador;
-                break;
                     printf("dados do aluno procurado:\n");
                     printf("matricula: %i\n", matricula[output]);
                     printf("nota: %.1f\n", nota[output]);
-
-                }else if(output == -1)
-                printf("matricula inexistente\n");
-
+                    break;
+                }
                 contador++;
+            } if(output == -1){
+                printf("matricula inexistente\n");
+                }
+        }
+        else if(opcao == 4){
+            for(i = 0; i <= indice - 1; i++){
+                    for(j = 0; j <= indice - i - 1; j++){
+                        if(matricula[j] > matricula[j + 1]){
+                            aux1 = matricula[j];
+                            matricula[j] = matricula[j+1];
+                            matricula[j+1] = aux1;
+                            aux2 = nota[j];
+                            nota[j] = nota[j+1];
+                            nota[j+1] = aux2;
+                        }
+                    }
+            }
+            printf("lista dos alunos com as matriculas ordenadas crescentemente:\n");
+            for(i = 0; i <= indice - 1; i++){
+            printf("%i aluno: %i  nota: %.1f\n", i+1, matricula[i], nota[i]);
+            }
+            flag4 = 1;
+        }
+        else if(opcao == 5){
+             if(flag4 == 1){
+                    printf("digite uma matricula p/ procurar: ");scanf("%i", &acharB);
+                    output= -1;
+                    li = 0;
+                    ls = indice - 1;
+                    meio = (li + ls) / 2;
+                    if(matricula[meio] < acharB)
+                        li = meio +1;
+            }
+            else if(flag4 == 0){
+                printf("ERRO: dados devem estar ordenados, execute opcao 4 primeiro\n");
             }
         }
 
